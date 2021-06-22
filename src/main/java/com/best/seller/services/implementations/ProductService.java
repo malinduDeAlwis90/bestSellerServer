c7ffff11391b30e59682d022fb83a33571119dab
Malindu <Malindu@CreativeSoftware.com>
2021-06-22 17:03:43 +0530

    Remove unused file

--- src/main/java/com/best/seller/services/implementations/ProductService.java
+++ src/main/java/com/best/seller/services/implementations/ProductService.java
@@ -9,7 +9,6 @@ import com.best.seller.services.blueprints.IProductService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
 
 import javax.persistence.EntityNotFoundException;
 import java.util.*;
